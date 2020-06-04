Fixed problems with variable replacement in e.g. convert_to_TF and scale_pars functions breaking TF graph connections and ruining gradient calculations. Still need to sort out 'nan's during NormalAnalysis fitting, and apparently wrong results of fit when using exact_MLEs (again just with NormalAnalysis I think)

JMCTF/binned_analysis.py
@@ -166,8 +166,8 @@ class BinnedAnalysis(BaseAnalysis):
         scaled_pars = {}
         scaled_nuis = {}
         #print("pars:",pars)
-        if 'nuisance' in pars.keys() and pars['nuisance'] is None:
-            # trigger shortcut to set nuisance parameters to zero, for sample generation. 
+        if 'theta' not in pars.keys():
+            # If values not provided, trigger shortcut to set nuisance parameters to zero. Useful for sample generation.
             theta_in = tf.constant(0*pars['s'])
         else:
             theta_in = pars['theta']

JMCTF/joint.py
@@ -7,6 +7,9 @@ from collections.abc import Mapping
 import massminimize as mm
 from . import common as c
 
+#tmp
+#id_only = False
+
 def neg2LogL(pars,const_pars,analyses,data,pre_scaled_pars,transform=None):
     """General -2logL function to optimise"""
     if transform is not None:
@@ -20,8 +23,9 @@ def neg2LogL(pars,const_pars,analyses,data,pre_scaled_pars,transform=None):
     print("neg2logL: all_pars:", all_pars)
     joint = JointDistribution(analyses.values(),all_pars,pre_scaled_pars)
     q = -2*joint.log_prob(data)
+    print("q:", q)
     if tf.math.reduce_any(tf.math.is_nan(q)):
-        msg = "NaNs detect in result of neg2LogL calculation! Please check that your input parameters are valid for the distributions you are investigating!"
+        msg = "NaNs detect in result of neg2LogL calculation! Please check that your input parameters are valid for the distributions you are investigating, and that the fit is stable!"
         raise ValueError(msg)
     total_loss = tf.math.reduce_sum(q)
     return total_loss, q, None, None
@@ -46,10 +50,10 @@ def optimize(pars,const_pars,analyses,data,pre_scaled_pars,transform=None,log_ta
               'pre_scaled_pars': pre_scaled_pars,
               'transform': transform
               }
-    print("pars:", pars)
-    print("const_pars:", const_pars)
+    print("pars:", pars) #c.print_with_id(pars,id_only))
+    print("const_pars:", const_pars) # c.print_with_id(const_pars,id_only))
 
-    exact_MLEs = False #True
+    exact_MLEs = False
     for a in analyses.values():
         if not a.exact_MLEs: exact_MLEs = False # TODO: check implementations 
     if exact_MLEs:
@@ -108,6 +112,8 @@ class JointDistribution(tfd.JointDistributionNamed):
                 self.Asamples.update(c.add_prefix(a.name,a.get_Asimov_samples(self.pars[a.name])))
             super().__init__(dists) # Doesn't like it if I use self.dists, maybe some construction order issue...
             self.dists = dists
+        else:
+            self.pars = None
         # If no pars provided can still fit the analyses, but obvious cannot sample or compute log_prob etc.
         # TODO: can we fail more gracefully if people try to do this?
         #       Or possibly the fitting stuff should be in a different object? It seems kind of nice here though.
@@ -117,16 +123,13 @@ class JointDistribution(tfd.JointDistributionNamed):
        out = {}
        #print("constant=",constant)
        for k,val in d.items():
-           if k is 'nuisance': # Special key we reserve for treating nuisance parameters.
-               out[k] = val
-               #print(k, 'was nuisance, left alone:', val)
-           elif tf.is_tensor(val):
-               out[k] = val # Already tensor-like, no need to convert (and in fact bad to do so since it could break graph connections)
-               #print(k, 'was tensor, left alone:', val)
+           if isinstance(val,tf.Variable):
+               out[k] = val # Already a Variable, no need to convert (and in fact bad to do so since it could break graph connections)
+               #print(k, 'was Variable, left alone:', val)
            elif isinstance(val, Mapping):
                out[k] = self.convert_to_TF(val,constant) # We must go deeper
            else:
-               # Try to create tensorflow variable from this data
+               # Try to create tensorflow variable or constant from this data
                try:
                    if constant:
                        out[k] = tf.constant(val, dtype=c.TFdtype)
@@ -167,7 +170,7 @@ class JointDistribution(tfd.JointDistributionNamed):
         scaled_pars = {}
         #print("pars:",pars)
         for a in self.analyses.values():
-            if a.name not in pars.keys(): raise KeyError("Parameters for analysis {0} not found!".format(a.name)) 
+            if a.name not in pars.keys(): raise KeyError("Parameters for analysis {0} not found!".format(a.name))
             s_pars, s_nuis, us_nuis = a.scale_pars(pars[a.name],pre_scaled_pars)
 
             # Logic to avoid applying scaling to parameters supplied with scaling already applied
@@ -199,8 +202,8 @@ class JointDistribution(tfd.JointDistributionNamed):
             p, fp = a.get_nuisance_tensorflow_variables(self.get_samples_for(a.name,samples),signal[a.name])
             pars[a.name] = p
             fixed_pars[a.name] = fp
-        print("pars:", pars)
-        print("fixed_pars:", fixed_pars)
+        #print("pars:", c.print_with_id(pars,id_only))
+        #print("fixed_pars:", c.print_with_id(fixed_pars,id_only))
         return pars, fixed_pars
 
     def get_samples_for(self,name,samples):
@@ -238,10 +241,10 @@ class JointDistribution(tfd.JointDistributionNamed):
         """Fit nuisance parameters to samples for a fixed signal
            (ignores parameters that were used to construct this object)"""
         sig = self.convert_to_TF(signal,constant=True)
-        print("sig:", sig)
+        print("sig:", sig) #c.print_with_id(sig,id_only))
         nuis_pars, fixed_pars = self.get_nuis_parameters(sig,samples)
-        print("nuis_pars:", nuis_pars)
-        print("fixed_pars:", fixed_pars)
+        print("nuis_pars:", nuis_pars) #c.print_with_id(nuis_pars,id_only))
+        print("fixed_pars:", fixed_pars) #c.print_with_id(fixed_pars,id_only))
         joint_fitted, q = optimize(nuis_pars,fixed_pars,self.analyses,samples,pre_scaled_pars='nuis',log_tag=log_tag,verbose=verbose)
         return q, joint_fitted, nuis_pars
 

JMCTF/normal_analysis.py
@@ -78,17 +78,17 @@ class NormalAnalysis(BaseAnalysis):
 
         #print("pars:", pars)
 
-        if 'nuisance' in pars.keys() and pars['nuisance'] is None:
-            # trigger shortcut to set nuisance parameters to zero, for sample generation. 
+        if 'theta' not in pars.keys():
+            # trigger shortcut to set nuisance parameters to zero. Useful for sample generation. 
             theta_in = tf.constant(0*pars['mu'])
-            if 'sigma_t' not in pars.keys():
-                # Default for when no extra "theory" uncertainty is provided
-                sigma_t_in = tf.constant(c.reallysmall,dtype=c.TFdtype) # Cannot use exactly zero, gets nan from TF due to zero width normal dist. Use something "near" smallest positive 32-bit float instead.
-            else:
-                sigma_t_in = pars['sigma_t'] 
         else:
             theta_in = pars['theta']
-            sigma_t_in = pars['sigma_t']
+
+        if 'sigma_t' not in pars.keys():
+            # Default for when no extra "theory" uncertainty is provided
+            sigma_t_in = tf.constant(c.reallysmall,dtype=c.TFdtype) # Cannot use exactly zero, gets nan from TF due to zero width normal dist. Use something "near" smallest positive 32-bit float instead.
+        else:
+            sigma_t_in = pars['sigma_t'] 
 
         scaled_pars['mu']    = pars['mu'] / self.mu_scaling
         scaled_nuis['theta'] = theta_in / self.theta_scaling
@@ -153,7 +153,8 @@ class NormalAnalysis(BaseAnalysis):
             sigma_t = c.reallysmall # TODO: Cannot use exactly zero 
         theta_MLE = - ((x - mu)*sigma_t**2 + x_theta*self.sigma**2) / (sigma_t**2 + self.sigma**2)
         pars = {"theta": tf.Variable(theta_MLE, dtype=c.TFdtype, name='theta')} # Use exact "starting guess", assuming mu is fixed.
-        fixed_pars = {"sigma_t": tf.constant(c.reallysmall, dtype=c.TFdtype, name='sigma_t')} # TODO: Cannot use exactly zero
+        fixed_pars = {"mu": tf.constant(mu, dtype=c.TFdtype, name='mu'), # mu fixed in nuisance-parameter-only fits
+                      "sigma_t": tf.constant(c.reallysmall, dtype=c.TFdtype, name='sigma_t')} # TODO: Cannot use exactly zero
         return pars, fixed_pars
 
     def get_all_tensorflow_variables(self,sample_dict):

docs/examples/quickstart.py
@@ -14,7 +14,7 @@ bins = [("SR1", 10, 9, 2),
         ("SR2", 50, 55, 4)]
 binned = BinnedAnalysis("Test binned", bins)
 # make_joint
-joint = JointDistribution([binned])
+joint = JointDistribution([norm,binned])
 #joint = JointDistribution([binned])
 # get_structure
 print("Sample structure:",joint.get_sample_structure()) 
@@ -47,7 +47,8 @@ print(to_numpy(all_pars_3))
 free, fixed, nuis = joint.get_parameter_structure()
 print("free:", free)
 
-null = {'Test normal': {'mu': [0.], 'nuisance': None}, 'Test binned': {'s': [(0., 0.)], 'nuisance': None}}
+null = {'Test normal': {'mu': [0.]}, 'Test binned': {'s': [(0., 0.)]}}
+
 joint_null = joint.fix_parameters(null)
 # or alternatively one can supply parameters to the constructor:
 # joint_null = JointDistribution([norm,binned],null)
@@ -91,8 +92,8 @@ ax.set_xlabel("LLR")
 ax.set(yscale="log")
 sns.distplot(LLR, color='b', kde=False, ax=ax, norm_hist=True, label="JMCTF")
 q = np.linspace(0, np.max(LLR),1000)
-chi2 = tf.math.exp(tfd.Chi2(df=4).log_prob(q)) 
-ax.plot(q,chi2,color='b',lw=2,label="chi^2 (DOF=4)")
+chi2 = tf.math.exp(tfd.Chi2(df=3).log_prob(q)) 
+ax.plot(q,chi2,color='b',lw=2,label="chi^2 (DOF=3)")
 ax.legend(loc=1, frameon=False, framealpha=0, prop={'size':10}, ncol=1)
 fig.tight_layout()
 fig.savefig("quickstart_LLR.svg")