Think I fixed up shape issues in Hessian calculation, but it is indeed too slow with the looping over samples. Need to try making it a tf.function for speed.

jmctf/LEE.py
@@ -852,6 +852,9 @@ class LEECorrectorAnalysis(LEECorrectorBase):
             samples = joint.sample(N)
             logw = tf.zeros((N,1),dtype=c.TFdtype)
 
+        print("in add_events: null_hyp:", self.null_hyp)
+        print("in add_events: samples:", samples)
+
         # Save events to database
         conn = self.connect_to_db()
         cur = conn.cursor()
@@ -1010,11 +1013,13 @@ class LEECorrectorAnalysis(LEECorrectorBase):
             i = 0
             pars = {}
             for par,size in nuis_structure.items():
-                pars[par] = tf.expand_dims(alldata[:,i:i+size],axis=1) # insert the 'alternate hypothesis' parameter dimension
+                #pars[par] = tf.expand_dims(alldata[:,i:i+size],axis=1) # insert the 'alternate hypothesis' parameter dimension
+                pars[par] = alldata[:,i:i+size]
                 i+=size
             nuis_pars = {self.analysis.name: pars}
         else:
             nuis_pars = None
+
         return nuis_pars
 
     def fit_alternate_batch(self,events,alt_hyp,altIDs=None,record_all=True):
@@ -1030,10 +1035,34 @@ class LEECorrectorAnalysis(LEECorrectorBase):
     def compute_quad(self,pars,events):
         """Compute quadratic approximations of profile likelihood for the specified
            set of events, expanding around the supplied nuisance parameter point with
-           the null hypothesis"""
+           the null hypothesis
+           
+           TODO: Having some shape confusions here. I *think* we need a set of nuisance
+           parameters for each event, e.g. the best fit nuisance parameters for each
+           event under (say) the background-only hypothesis. So need to make sure pars
+           and events supplied have dimensions consistent with this.
+        """
+
+        par_size = c.get_size(pars,axis=0)
+
+        # Squeeze out the "hypothesis list" axis from events, to give output Hessians
+        # a better shape.
+        # TODO: Should only be events for one hypothesis given as input. Check shape
+        events_sq = c.deep_squeeze(events,axis=1)
+        e_size = c.get_size(events_sq,axis=0)
+
+        if par_size is None or e_size is None:
+            raise ValueError("pars or events were empty! pars={0}, events={1}".format(pars,events))
+
+        if par_size!=e_size:
+            msg = "Parameters and events did not have sizes consistent for compute_quad: axis 0 size must match!"
+            raise ValueError(msg)
+
+        print("pars:", pars)
+        print("events_sq:", events_sq)
+
         joint_fitted_b = JointDistribution([self.analysis],c.deep_merge(self.null_hyp,pars))
-        print("self.null_hyp:",self.null_hyp)
-        quadf = joint_fitted_b.quad_loglike_f(events)
+        quadf = joint_fitted_b.quad_loglike_f(events_sq)
 
         return quadf 
 
@@ -1312,7 +1341,6 @@ class LEECorrectorAnalysis(LEECorrectorBase):
                     for i in range(arr.shape[-1]):
                         cols += ["{0}_{1}".format(par,i)]
                     arrays += [fix_dims(arr)] # remove 'alternate hypothesis' dimension, if needed
-                print("arrays:", arrays)
                 allpars = tf.concat(arrays,axis=-1)               
                 data = pd.DataFrame(allpars.numpy(),index=EventIDs.numpy(),columns=cols)
                 data.index.name = 'EventID' 

jmctf/common.py
@@ -227,11 +227,11 @@ def flatten(container):
             else:
                 yield i
 
-def atleast_2d(pars,report=False):
+def atleast_2d(pars,report=False,new1Daxis=1):
     """Ensure parameters in nested dictionaries meet the shape conventions of JointDistribution, i.e.
        all pars must be 2D (as expanded by atleast_2d)
-       Doesn't work quite like the numpy version: new dimensions
-       for 1D input are added at axis 1 rather than 0.
+       Doesn't work quite like the numpy version by default: new dimensions
+       for 1D input are added at axis 1 rather than 0. But setting new1Daxis=0 gives numpy behaviour.
        If 'report' is True, also return a flag indicating whether any dimension expansion occured. 
     """
     did_expansion = False
@@ -245,7 +245,7 @@ def atleast_2d(pars,report=False):
         out = tf.expand_dims(tf.expand_dims(pars,axis=0),axis=0)
         did_expansion = True
     elif len(pars.shape)==1:
-        out = tf.expand_dims(pars,axis=1)
+        out = tf.expand_dims(pars,axis=new1Daxis)
         did_expansion = True               
     else:
         out = pars
@@ -306,7 +306,7 @@ def get_size(d,axis):
         size = -1
         for k,v in d.items():
             size_v = get_size(v,axis)
-            if size==-1: 
+            if size==-1 or size==1: # size 1 can be broadcast to larger sizes, so this is ok to be different 
                 size = size_v
             elif size!=size_v:
                 size = None
@@ -323,7 +323,7 @@ def squeeze_axis_0(pars):
     pars_2d = atleast_2d(pars)
     size_axis0 = get_size(pars,axis=0)
     if size_axis0==1:
-        out = _squeeze_axis_0_inner(pars)
+        out = deep_squeeze(pars,axis=0)
     elif size_axis0==None:
         msg = "Error while squeezing axis 0 for objects in nested dictionary! Objects did not have consistent size of axis 0"
         raise ValueError(msg) 
@@ -331,13 +331,14 @@ def squeeze_axis_0(pars):
         out = pars # Do nothing to parameters, not even the shape adjustment
     return out
 
-def _squeeze_axis_0_inner(pars):
+def deep_squeeze(pars,axis):
+    """Apply tf.squeeze to all bottom-level objects in nested dictionaries"""
     if isinstance(pars, Mapping):
         out = {}
         for k,v in pars.items():
-            out[k] = _squeeze_axis_0_inner(v)
+            out[k] = deep_squeeze(v,axis)
     else:
-        out = tf.squeeze(pars,axis=0) # Should be pre-checked to be size 1
+        out = tf.squeeze(pars,axis=axis) # Should be pre-checked to be size 1
     return out
   
 def loose_squeeze(tensor,axis):

jmctf_tests/exploratory_tests/testing_LEEcorrector.py
@@ -79,12 +79,12 @@ class SigGen:
         self.j += size
         return chunk, list(range(j,j+size))
 
-nosignal = {a.name: {'s': tf.constant([[0. for sr in a.SR_names]],dtype=float)} for a in analyses_read}
+nosignal = {a.name: {'s': tf.constant([tuple(0. for sr in a.SR_names)],dtype=float)} for a in analyses_read}
 DOF = 3
 
 # Specific signal whose local distributions and p-values we would like to know
 # This case is 'cherry-picked' to match the "observed" counts in every signal region of every analysis
-signal_test = {a.name: {'s': tf.constant([[n for n in a.SR_n]], dtype=float)} for a in analyses_read}
+signal_test = {a.name: {'s': tf.constant([tuple(n for n in a.SR_n)], dtype=float)} for a in analyses_read}
 
 path = 'TEST'
 master_name = 'all'