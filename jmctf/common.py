Might have fixed Hessian batching, still testing

jmctf/common.py
@@ -38,6 +38,25 @@ def to_numpy(d):
         else: out[k] = v.numpy()
         return out
 
+def deep(d_arg=0):
+    def deep_decorator(f):
+        """Decorator to make a function apply over the
+           bottom level of nested dictionaries.
+           d_arg specified which argument of the
+           function is to be treated as the dictionary"""
+        def deep_f(*args,**kwargs):
+            if isinstance(args[d_arg], Mapping):
+                out = {}
+                for k,v in args[d_arg].items():
+                    args_list = list(args)
+                    args_list[d_arg] = v
+                    out[k] = deep_f(*args_list,**kwargs)
+            else:
+                out = f(*args,**kwargs)
+            return out
+        return deep_f
+    return deep_decorator
+
 def adapt_array(arr):
     """
     http://stackoverflow.com/a/31312102/190597 (SoulNibbler)
@@ -282,12 +301,14 @@ def prepare_par_shapes(pars,squeeze=True):
         pars_out = pars_2d  
     return pars_out, did_expansion
 
-def extract_ith(d,i):
-    """Extract ith entry from bottom level of all nested dictionaries"""
+def extract_ith(d,i,keep_axis=False):
+    """Extract ith entry from bottom level of all nested dictionaries.
+       If keep_axis=True then the axis from which the entry is extracted
+       is kept as a singleton dimension."""
     if isinstance(d,Mapping):
         out = {}
         for k,v in d.items():
-            out[k] = extract_ith(v,i)
+            out[k] = extract_ith(v,i,keep_axis)
     elif d.shape==():
         if i==0:
             # We will allow extraction of a scalar using index 0
@@ -295,6 +316,8 @@ def extract_ith(d,i):
         else:
             msg = "Tried to extract from a scalar using index other than zero! Index was {0}, bottom-level item was {1}".format(i,d)
             raise IndexError(msg)
+    elif keep_axis:
+        out = d[i:i+1]
     else:
         out = d[i]
     return out
@@ -331,15 +354,15 @@ def squeeze_axis_0(pars):
         out = pars # Do nothing to parameters, not even the shape adjustment
     return out
 
+@deep(1)
+def deep_einsum(instructions,d):
+    """Perform a tf.einsum on the bottom level of nested dictionaries"""
+    return tf.einsum(instructions,d)
+
+@deep(0)
 def deep_squeeze(pars,axis):
     """Apply tf.squeeze to all bottom-level objects in nested dictionaries"""
-    if isinstance(pars, Mapping):
-        out = {}
-        for k,v in pars.items():
-            out[k] = deep_squeeze(v,axis)
-    else:
-        out = tf.squeeze(pars,axis=axis) # Should be pre-checked to be size 1
-    return out
+    return tf.squeeze(pars,axis=axis) # Should be pre-checked to be size 1
   
 def loose_squeeze(tensor,axis):
     """Applies squeeze to bottom-level dict objects along axis, but isn't an error if the axis
@@ -416,7 +439,7 @@ def uncat_pars_2d(catted_pars,pars_template,axis0_removed=False):
                 piN = tf.squeeze(catted_pars_2d[...,i:i+N],axis=[0,1])
             elif len(p.shape)==1:
                 N = 1 # Assume there is an implicit singleton inner dimension
-                piN = tf.squeeze(catted_pars_2d[...,i:i+N],axis=[0])        
+                piN = catted_pars_2d[...,i:i+N]        
             else:
                 # No change to shape required
                 N = p.shape[-1]

jmctf/joint.py
@@ -570,9 +570,12 @@ class JointDistribution(tfd.JointDistributionNamed):
             msg = "Parameters and events did not have sizes consistent for Hessian calculation: axis 0 size must match!"
             raise ValueError(msg)
 
-        # Need to adjust sample shapes to make Hessian output nicer
-        # Squeeze out singleton dimensions.
+        # Need to adjust sample shapes so that they are interpreted by tensorflow_probability
+        # as "batches", one for each entry of 'pars', rather than as a single batch of many 'events'.
+        # In this case it just means swapping the first two dimensions
         print("Hessian: samples:", samples)
+        batch_samples = c.deep_einsum("ij...->ji...",samples)
+        print("batch_samples:", batch_samples)
  
         # Separate "const" parameters, and also adjust shapes of parameters
         # to make life easier later
@@ -589,7 +592,7 @@ class JointDistribution(tfd.JointDistributionNamed):
                     free_pars[a][name] = v
 
         # Make sure shape of const parameters is correct
-        const_pars0 = c.extract_ith(c.atleast_2d(const_pars),0)
+        const_pars_2d = c.atleast_2d(const_pars)
 
         # Stack current parameter values to single tensorflow variable for
         # easier matrix manipulation
@@ -619,60 +622,50 @@ class JointDistribution(tfd.JointDistributionNamed):
         # pre-built graph
         hessian_list = []
         grad_list = []
-        for x,input_pars_slice in zip(c.iterate_samples(samples),all_input_pars):
-            input_pars = tf.Variable(input_pars_slice)
-            #x = c.loose_squeeze(xi,axis=0) # Remove leading singleton (hypothesis) dimension to improve Hessian shape
-            with tf.GradientTape(persistent=True,watch_accessed_variables=False) as tape:
+        #for x,input_pars_slice in zip(c.iterate_samples(samples),all_input_pars):
+        input_pars = tf.Variable(all_input_pars)
+        #x = c.loose_squeeze(xi,axis=0) # Remove leading singleton (hypothesis) dimension to improve Hessian shape
+        with tf.GradientTape() as tape_outer:
+            with tf.GradientTape() as tape:
+            #with tf.GradientTape(persistent=True,watch_accessed_variables=False) as tape:
                 tape.watch(input_pars)
                 # Don't need to go via JointDistribution, can just
                 # get log_prob for all component dists "manually"
                 # Avoids confusion about parameters getting copied and
                 # breaking TF graph connections etc.
-                print("x:", x)
+                print("samples:", samples)
                 print("free_pars:", free_pars)
-                catted_pars = tf.expand_dims(input_pars,axis=0) # Put singleton hypothesis axis back in for de-catting later
                 print("input_pars:", input_pars)
-                print("catted_pars:", catted_pars)
-                inpars = c.uncat_pars_2d(catted_pars,free_pars) # need to unstack for use in each analysis
-                # Extract 0th hypothesis from unstacked pars
-                # Basically removes 0th dimension again to improve generated samples shape and thus Hessian shape.
-                inpars0 = c.extract_ith(inpars,0) 
-             
+                #print("catted_pars:", catted_pars)
+                inpars = c.uncat_pars_2d(input_pars,free_pars) # need to unstack for use in each analysis
+         
                 # print("inpars:", inpars)
                 # merge with const parameters
-                all_inpars = c.deep_merge(inpars0,const_pars0)
+                all_inpars = c.deep_merge(inpars,const_pars_2d)
                 scaled_inpars = self.scale_pars(all_inpars)
                 q = 0
                 for a in self.analyses.values():
                     d = c.add_prefix(a.name,a.tensorflow_model(scaled_inpars[a.name])) 
                     for dist_name, dist in d.items():
-                        print("x[{0}]:".format(dist_name), x[dist_name])
-                        q += -2*dist.log_prob(x[dist_name])
-                grads = tape.gradient(q, catted_pars)[0]
-                #grads = tape.jacobian(q, catted_pars)
-                #grads = tape.batch_jacobian(q, catted_pars)
-                #grads = tape.hessians(q, catted_pars)
-                print("samples:", samples)
-                print("all_inpars:", all_inpars)
-                # print("scaled_inpars:", scaled_inpars)
-                # print("catted_pars:", catted_pars)
+                        print("x[{0}]:".format(dist_name), batch_samples[dist_name])
+                        q += -2*dist.log_prob(batch_samples[dist_name])
                 print("q:", q)
-                print("grads:", grads)
-            # Compute Hessians. batch_jacobian takes first (the sample) dimensions as independent for much better efficiency,
-            #hessians = tape.batch_jacobian(grads, catted_pars) 
-            #...but we are only allowing one sample anyway, so can just do normal jacobian
-            hessian = tape.jacobian(grads, input_pars)
-            #print("H:",hessians)
-            grad_list += [grads]
-            hessian_list += [hessian]
-        print("hessian_list:", hessian_list)
-        grads = tf.stack(grad_list,axis=0)
-        hessians = tf.stack(hessian_list,axis=0)
-      
-        # Remove the singleton dimensions. We should not be computing Hessians for batches of signal hypotheses.
-        #grads_out = tf.squeeze(grads,axis=[-2])
-        #hessians_out = tf.squeeze(hessians,axis=[-2,-4])
-        # ... not very robust. Need to instead fix up shapes of input parameters I think.
+            grads = tape.gradient(q, input_pars) #[0]
+            #grads = tape.jacobian(q, catted_pars)
+            #grads = tape.batch_jacobian(q, catted_pars)
+            #grads = tape.hessians(q, catted_pars)
+            print("batch_samples:", batch_samples)
+            print("all_inpars:", all_inpars)
+            # print("scaled_inpars:", scaled_inpars)
+            # print("catted_pars:", catted_pars)
+            print("q:", q)
+            print("grads:", grads)
+        # Compute Hessians. batch_jacobian takes first (the sample) dimensions as independent for much better efficiency,
+        hessians = tape_outer.batch_jacobian(grads, input_pars) 
+        #...but we are only allowing one sample anyway, so can just do normal jacobian
+        #hessian = tape_outer.jacobian(grads, input_pars)
+        print("H:",hessians)
+     
         grads_out = grads
         hessians_out = hessians
 