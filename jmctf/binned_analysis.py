Progress with shape propagation in Hessian; now need to fix up some stuff about how parameter shapes are namd in db files in LEE analyses

jmctf/binned_analysis.py
@@ -239,22 +239,25 @@ class BinnedAnalysis(BaseAnalysis):
             Osamples["x"] = tf.expand_dims(tf.expand_dims(tf.constant([0]*len(self.SR_names),dtype=c.TFdtype),0),0)
         return Osamples
 
-    def get_interest_parameter_structure(self):
+    def interest_parameter_shapes(self):
         """Get a dictionary describing the structure of the "interesting" parameters in this analysis
-           Basically just the keys of the parameter dictionaries plus dimension of each entry"""
-        return {"s": len(self.SR_n)}
+           Basically just the keys of the parameter dictionaries plus primitive (non-batch dimensions) 
+           shape of each entry"""
+        return {"s": (len(self.SR_n),)}
 
-    def get_fixed_parameter_structure(self):
+    def fixed_parameter_shapes(self):
         """Get a dictionary describing the structure of the fixed parameters in this analysis
            (i.e. parameters that can be altered along with the signal hypothesis in nuisance
            parameter fits, but which are kept fixed during all fitting.
-           Basically just the keys of the parameter dictionaries plus dimension of each entry"""
+           Basically just the keys of the parameter dictionaries plus primitive (non-batch dimensions) 
+           shape of each entry"""
         return {}
 
-    def get_nuisance_parameter_structure(self):
+    def nuisance_parameter_shapes(self):
         """Get a dictionary describing the nuisance parameter structure of this analysis.
-           Basically just the keys of the parameter dictionaries plus dimension of each entry"""
-        return {"theta": len(self.SR_b)} # Just one nuisance parameter per signal region, packaged into one tensor. 
+           Basically just the keys of the parameter dictionaries plus primitive (non-batch dimensions) 
+           shape of each entry"""
+        return {"theta": (len(self.SR_b),)} # Just one nuisance parameter per signal region, packaged into one 1D tensor. 
 
     def get_nuisance_parameters(self,sample_dict,fixed_pars):
         """Get nuisance parameters to be optimized, for input to "tensorflow_model"""

jmctf/common.py
@@ -5,6 +5,7 @@ import sqlite3
 import numpy as np
 import tensorflow as tf
 import scipy.interpolate as spi
+from functools import reduce
 from collections.abc import Mapping
 
 # Reference dtype for consistency in TensorFlow operations
@@ -330,8 +331,8 @@ def deep_shape(d,axis=0,numpy_bcast=True):
 def get_bcast_shape(shape1,shape2,numpy_bcast=True):
     """Of two tensor/array shapes, return the shape to which they can be broadcast under numpy rules
        (or throw an error if bcast is not possible"""
-    print("shape1:", shape1, len(shape1))
-    print("shape2:", shape2, len(shape2))
+    #print("shape1:", shape1, len(shape1))
+    #print("shape2:", shape2, len(shape2))
     if len(shape1)!=len(shape2):
         if numpy_bcast:
             # Apply numpy broadcasting rules to match dimensions and add "implicit" dimensions
@@ -349,8 +350,8 @@ def get_bcast_shape(shape1,shape2,numpy_bcast=True):
             # Without full numpy broadcasting, shape is indeterminate 
             msg = "Shapes are not compatible! They have different numbers of dimensions (found {0} vs {1})".format(shape1,shape2)
             raise ValueError(msg)
-    print("expanded shape1:", shape1, len(shape1))
-    print("expanded shape2:", shape2, len(shape2))
+    #print("expanded shape1:", shape1, len(shape1))
+    #print("expanded shape2:", shape2, len(shape2))
     # Number of dimensions should now match. Now compare their sizes.
     newshape = [-1 for s in shape1]
     for i,(si,sv) in enumerate(zip(shape1,shape2)):
@@ -366,7 +367,7 @@ def get_bcast_shape(shape1,shape2,numpy_bcast=True):
     if -1 in newshape:
         msg = "Failed to broadcast shapes! -1 detected! (shape1 = {0}, shape2={1}, newshape = {2})".format(shape1,shape2,newshape)
         raise ValueError(msg)
-    print("newshape: {0}".format(newshape))
+    #print("newshape: {0}".format(newshape))
     return tuple(newshape)
 
 def squeeze_axis_0(pars):
@@ -466,7 +467,7 @@ def sample_batch_shape(sample,event_shapes):
         if len(eshape)>0 and x.shape[-len(eshape):] != eshape:
             msg = "Supplied event shape for distribution {0} ({1}) does not match trailing dimensions of sample! ({2})!".format(name,eshape,x.shape)
             raise ValueError(msg)
-        bshape = x.shape[:-len(eshape)]
+        bshape = x.shape[:-len(eshape) or None]
         if batch_shape is None:
             batch_shape = bshape
         elif batch_shape!=bshape:
@@ -487,78 +488,98 @@ def loose_squeeze(tensor,axis):
         out = tensor
     return out
 
-def cat_pars_2d(pars,remove_axis0=False):
-    """Stack separate tensorflow parameters from a dictionary into
-       a single tensor, in order fixed by the dict default iteration order.
-       Ensures that output is at least 2D, i.e. scalar input has two dimensions
-       added, and 1D input has one dimension added (as axis 0).
-       If remove_axis0 is True, then AFTER expanding everything to 2D, axis 0
-       is squeezed. It will be an error to use this option for input for which
-       the (expanded) axis 0 is not a singleton.
+def cat_pars_to_tensor(pars,par_shapes):
+    """Combine a dictionary of parameters into a single tensor,
+       broadcasting all batch_shape dimensions against each
+       other and then flattening them to 1D, and flattening
+       all "parameter_shape" dimensions before concatenating
+       everything to a single 2D tensor.
+       Operation reversed (broadcast is not undone) by
+       decat_tensor_to_pars
 
-       TODO: Allow 2D parameters? Could automatically flatten/unflatten them to 1D.
+       Error thrown if broadcast is not possible.
+
+       par_shapes should be a dictionary matching the structure
+       of "pars", where the bottom level objects are lists/tuples/tensors
+       that describe the primitive (i.e. non batch shape) dimensions
+       of each parameters (as returned by the parameter_shapes()
+       method for each analysis)
     """
-    parlist = []
-    maxdims = {}
-    for ka,a in atleast_2d(pars).items():
-        for kp,p in a.items():
-            if remove_axis0:
-                if p.shape[0]!=1:
-                    msg = "Tried to squeeze axis 0 due to 'remove_axis0=True' flag, but this axis was not a singleton for parameter {0} of analysis {1}. This parameter had original shape {2} and expanded shape {3}".format(kp,ka,p.shape,p.shape)
-                    raise ValueError(msg)
-                else:
-                    p = tf.squeeze(p,axis=0)
-            parlist += [p]
-            i = -1
-            for d in p.shape[::-1]:
-                if i not in maxdims.keys() or maxdims[i]<d: maxdims[i] = d
-                i-=1
-    maxshape = [None for i in range(len(maxdims))]
-    for i,d in maxdims.items():
-        maxshape[i] = d
 
-    # Attempt to broadcast all inputs to same shape
+    # Will attempt to broadcast all inputs to same batch shape, so need
+    # to first find this shape.
+    max_batch_shape = ()
+
+    for ka,a in pars.items():
+        for kp,p in a.items():
+            # Compare par_shapes to trailing dims and check for consistency
+            if ka not in par_shapes.keys():
+                msg = "Failed to form tensor from input parameters! 'par_shapes' dictionary did not contain any shapes for analysis {0} found in 'pars'. par_shapes was: {1}".format(ka,par_shapes)
+                raise ValueError(msg)
+            elif kp not in par_shapes[ka].keys():
+                msg = "Failed to form tensor from input parameters! 'par_shapes' dictionary did not contain the basic shape of parameter {0} in analysis {1}! par_shapes was: {2}".format(kp,ka,par_shapes)
+                raise ValueError(msg)
+                
+            n = len(par_shapes[ka][kp])
+
+            if p.shape[-n:] != par_shapes[ka][kp]:
+                msg = "Failed to form tensor from input parameters! The trailing dimensions of parameter {0} (with shape={1}) do not match the shape specified for this parameter in the par_shapes dictionary (which was {2})".format(kp,p.shape,par_shapes[ka][kp])
+
+            batch_shape = p.shape[:-n or None] # The 'or None' deals with the n=0 case nicely. 
+            max_batch_shape = get_bcast_shape(batch_shape,max_batch_shape)
+            #print("p:{0}, p.shape:{1}, par_shape:{2}, n:{3}, par_batch:{4}, bcast_batch: {5}".format(kp,p.shape,par_shapes[ka][kp],n,batch_shape,max_batch_shape))
+
+    # Consistent batch shape discovered. Create list of broadcasted parameters, with flattened batch_shape and par_shape
     matched_parlist = []
-    bcast = tf.broadcast_to(tf.constant(np.ones([1 for d in range(len(maxdims))]),dtype=TFdtype),maxshape)
-    for p in parlist:
-        matched_parlist += [p*bcast]
-    #return tf.Variable(tf.concat(matched_parlist,axis=-1),name="all_parameters")               
-    return tf.concat(matched_parlist,axis=-1) 
-
-def uncat_pars_2d(catted_pars,pars_template,axis0_removed=False):
-    """De-stack tensorflow parameters back into separate variables of
-       shapes know to each analysis. Assumes stacked_pars are of the
-       same structure as pars_template. This version reverses
-       cat_pars_2d, i.e. it removes singleton dimensions as found to
-       match scalar and 1D entries in pars_template.
-       If axis0_removed is True, it is assumed that the catted_pars
-       have had their 0th axis squeezed, and this is restored during
-       the de-catting"""
-    pars = {}
-    i = 0
-    if axis0_removed:
-        # Restore singleton axis 0 if it was removed
-        catted_pars_2d = tf.expand_dims(catted_pars,axis=0)              
-    else:
-        catted_pars_2d = catted_pars
-
-    for ka,a in pars_template.items():
-        pars[ka] = {}
+    
+    for ka,a in pars.items():
         for kp,p in a.items():
-            # We always stack to at least 2D, so need to undo this for some cases
-            if p.shape==():
-                N = 1 # Even scalars still take up one slot
-                piN = tf.squeeze(catted_pars_2d[...,i:i+N],axis=[0,1])
-            elif len(p.shape)==1:
-                N = 1 # Assume there is an implicit singleton inner dimension
-                piN = catted_pars_2d[...,i:i+N]        
-            else:
-                # No change to shape required
-                N = p.shape[-1]
-                piN = catted_pars_2d[...,i:i+N]
-            pars[ka][kp] = piN
-            i+=N
-    return pars
+            bcast_shape = [d for d in max_batch_shape] + [d for d in par_shapes[ka][kp]]
+            #print("Attempting to broadcast par {0} (value={1}) with shape {2} to shape {3}".format(kp,p,p.shape,bcast_shape))
+            bcast_par = tf.broadcast_to(p,bcast_shape)
+
+            # Reshape to 2D. I think this should just work...
+            new_shape = [prod(max_batch_shape),prod(par_shapes[ka][kp])]
+            #print("bcast_par:", bcast_par)
+            #print("max_batch_shape:", max_batch_shape)
+            #print("new_shape:", new_shape)
+            #print("Attempting to reshape bcast_par {0} with shape {1} to shape {2}".format(kp,bcast_par.shape,new_shape)) 
+            pars_2D = tf.reshape(bcast_par,new_shape)
+            matched_parlist += [pars_2D]
+    
+    # Do the final concatenation along the "parameter" axis
+    return tf.concat(matched_parlist,axis=1), max_batch_shape
+
+def decat_tensor_to_pars(tensor,par_template,par_shapes,batch_shape):
+    """Undo cat_pars_to_tensor, assuming tensor was constructed using
+       the order inferred from par_template dictionary 
+       (doesn't undo broadcasting)"""
+    #print("tensor:", tensor)
+    out_pars = {}
+    i = 0
+    for ka,a in par_template.items():
+        out_pars[ka] = {}
+        for kp,p in a.items():
+            if ka not in par_shapes.keys():
+                msg = "Failed to extract parameters from input tensor! 'par_shapes' dictionary did not contain any shapes for analysis {0} found in 'par_template'. par_shapes was: {1}".format(ka,par_shapes)
+                raise ValueError(msg)
+            elif kp not in par_shapes[ka].keys():
+                msg = "Failed to extract parameters from input tensor! 'par_shapes' dictionary did not contain the basic shape of parameter {0} in analysis {1}! par_shapes was: {2}".format(kp,ka,par_shapes)
+                raise ValueError(msg)            
+            n = prod(par_shapes[ka][kp])
+            par_slice = tensor[:,i:i+n]
+            #print("par_slice:", par_slice)
+            new_shape = [d for d in batch_shape] + [d for d in par_shapes[ka][kp]]
+            #print("batch_shape:", batch_shape)
+            #print("par_shapes:", par_shapes[ka][kp])
+            #print("new_shape:", new_shape)
+            out_pars[ka][kp] = tf.reshape(par_slice,new_shape)
+            i+=n
+    return out_pars
+   
+def prod(iterable,initializer=1):
+    """Take product of all elements"""
+    return reduce(lambda x, y: x*y, iterable, initializer)
 
 def iterate_samples(sample_dict):
     """Iterate through samples in a dictionary of samples"""

jmctf/joint.py
@@ -454,19 +454,12 @@ class JointDistribution(tfd.JointDistributionNamed):
             raise ValueError(msg)
         return pars, fixed_pars
 
-    def get_sample_structure(self):
-        """Returns a dictionary whose structure is the same as samples from the joint PDF"""
-        out = {}
-        for a in self.analyses.values():
-            out.update(c.add_prefix(a.name,a.get_sample_structure()))
-        return out
-
-    def get_parameter_structure(self):
+    def decomposed_parameter_shapes(self):
         """Returns three dictionaries whose structure explains how parameters should be supplied
            to this object"""
-        interest  = {a.name: a.get_interest_parameter_structure() for a in self.analyses.values()}
-        fixed = {a.name: a.get_fixed_parameter_structure() for a in self.analyses.values()}
-        nuis  = {a.name: a.get_nuisance_parameter_structure() for a in self.analyses.values()} 
+        interest  = {a.name: a.interest_parameter_shapes() for a in self.analyses.values()}
+        fixed = {a.name: a.fixed_parameter_shapes() for a in self.analyses.values()}
+        nuis  = {a.name: a.nuisance_parameter_shapes() for a in self.analyses.values()} 
         return interest, fixed, nuis
 
     def fit_nuisance(self,samples,fixed_pars=None,log_tag='',verbose=False):
@@ -545,36 +538,25 @@ class JointDistribution(tfd.JointDistributionNamed):
         """Obtain Hessian matrix (and grad) at input parameter points
            Make sure to use de-scaled parameters as input!
 
-           Shape requirements: pars must either be a *single* hypothesis,
-           or else a number of hypotheses equal to the number of samples
-           (e.g. for when we want one Hessian per sample, expanded around
-           some parameter point tailored to each sample).
+           Shape requirements: 
+            samples should match self.pars, in the sense that the 
+            sample_dims+batch_dims for the samples should match the
+            batch_dims that results from self.pars, after broadcasting.
+            That is, the samples should be matched one-to-one to (broadcast) 
+            parameters.
 
-           NOTE: Actually for now we only allow the second option.
+            (e.g. we want one Hessian per sample, expanded around
+            some parameter point tailored to each sample).
 
            Parameters should be those already known internally to this
            JointDistribution.
         """
 
         # Check batch shapes associated with internal probability models
-        # This will affect the output Hessian shape, and we need to correct for it.
+        # This will affect the output Hessian shape, i.e. we 
         # Returns a dict of batch shapes.
-        batch_shapes = self.batch_shape_tensor()
- 
-        # The batch shapes should all be consistent!
-        consistent, batch_shape = c.deep_equals(batch_shapes)
- 
-        print("Hessian: batch_shapes:", batch_shapes)
+        batch_shape = self.bcast_batch_shape_tensor()
         print("Hessian: batch_shape:", batch_shape)
-
-        # Throw error if batch shape cannot be compressed to 1D
-        dims_to_squeeze = tf.where(tf.equal(batch_shape,1))[:,0]
-        print("dims_to_squeeze:", dims_to_squeeze) 
-
-        print(dims_to_squeeze.shape[0] - batch_shape.shape[0])
-        if batch_shape.shape[0]!=1 and (batch_shape.shape[0] - dims_to_squeeze.shape[0]) != 1:
-            msg = "Disallowed batch shape observed! For Hessian calculation we ultimated require 1D batches of hypotheses and samples to be matched, so any batch shape that cannot be 'squeezed' to 1D is not permitted. Please check the shapes of the supplied parameters."
-            raise ValueError(msg)
 
         # Make sure to use non-scaled parameters to get correct gradients etc.
         pars = self.descale_pars(self.pars)
@@ -592,12 +574,14 @@ class JointDistribution(tfd.JointDistributionNamed):
                 else:
                     free_pars[a][name] = v
 
-        # Make sure shape of const parameters is correct
-        const_pars_2d = c.atleast_2d(const_pars) # Do we really need this?
+        # Stack parameters into a single tensorflow variable for
+        # matrix manipulations
+        par_shapes = self.parameter_shapes()
+        all_input_pars, bcast_batch_shape = c.cat_pars_to_tensor(free_pars,par_shapes)
 
-        # Stack current parameter values to single tensorflow variable for
-        # easier matrix manipulation
-        all_input_pars = c.cat_pars_2d(free_pars) # Our input variables to be traced. 
+        if bcast_batch_shape != batch_shape:
+            msg = "Broadcasted batch shape inferred while stacking parameters into tensor did not match batch shape inferred from underlying distribution objects! This is a bug, if there is a problem with the input parameters it should have been detected before this."
+            raise ValueError(msg)
 
         input_pars = tf.Variable(all_input_pars)
         with tf.GradientTape() as tape_outer:
@@ -612,11 +596,11 @@ class JointDistribution(tfd.JointDistributionNamed):
                 #print("free_pars:", free_pars)
                 #print("input_pars:", input_pars)
                 #print("catted_pars:", catted_pars)
-                inpars = c.uncat_pars_2d(input_pars,free_pars) # need to unstack for use in each analysis
+                inpars = c.decat_tensor_to_pars(input_pars,free_pars,par_shapes,batch_shape) # need to unstack for use in each analysis
          
                 # print("inpars:", inpars)
                 # merge with const parameters
-                all_inpars = c.deep_merge(inpars,const_pars_2d)
+                all_inpars = c.deep_merge(inpars,const_pars)
                 scaled_inpars = self.scale_pars(all_inpars)
                 q = 0
                 for a in self.analyses.values():
@@ -642,19 +626,21 @@ class JointDistribution(tfd.JointDistributionNamed):
         #hessian = tape_outer.jacobian(grads, input_pars)
         print("H:",hessians)
 
-        # If Hessian (or grad) dimension is too large (due to extra singleton dimensions in either the samples or the
-        # input parameters) then squeeze them out until we get to the right shape.
-        hessians_out = c.squeeze_to(hessians,d=3,dont_squeeze=[0])
-        grads_out = c.squeeze_to(grads,d=2,dont_squeeze=[0])
-        print("g_out:",grads_out)
-        print("H_out:",hessians_out)
+        # # If Hessian (or grad) dimension is too large (due to extra singleton dimensions in either the samples or the
+        # # input parameters) then squeeze them out until we get to the right shape.
+        # hessians_out = c.squeeze_to(hessians,d=3,dont_squeeze=[0])
+        # grads_out = c.squeeze_to(grads,d=2,dont_squeeze=[0])
+        # print("g_out:",grads_out)
+        # print("H_out:",hessians_out)
+        hessians_out = hessians
+        grads_out = grads
         return hessians_out, grads_out
 
     def decomposed_parameters(self,pars):
         """Separate input parameters into 'interest' and 'nuisance' lists,
            keeping tracking of their original 'indices' w.r.t. catted format.
            Mainly used for decomposing Hessian matrix."""
-        interest, fixed, nuisance = self.get_parameter_structure()
+        interest, fixed, nuisance = self.decomposed_parameter_shapes()
         interest_i = {} # indices of parameters in Hessian/stacked pars
         nuisance_i = {}
         interest_p = {} # parameters themselves
@@ -776,44 +762,56 @@ class JointDistribution(tfd.JointDistributionNamed):
     def neg2loglike_quad(self,signal,A,B,interest,nuisance,samples):
         """Compute -2*loglikelihood using pre-computed Taylor expansion
            parameters (for many samples) for a set of signal hypotheses"""
-        # Stack signal parameters into appropriate vector for matrix operations
-        parlist = []
+
+        # Make sure format of signal parameters matches the known "interest" parameters
+        # for all analyses (also culls out any unnecessary parameters)
+        parlist = {}
         for ka,a in interest.items():
             if ka not in signal.keys():
-                raise ValueError("No test signals provided for analysis {0}".format(ka)) 
+                raise ValueError("No test signals provided for analysis {0}".format(ka))
+            parlist[ka] = {}
             for kp in a.keys():
                 if kp not in signal[ka].keys():
                     raise ValueError("No test signals provided for parameter {0} in analysis {1}".format(kp,ka))
                 #print("signal...", signal[ka][kp])
-                parlist += [signal[ka][kp]]
+                parlist[ka][kp] = signal[ka][kp]
 
         if A is None or B is None:
             # No nuisance parameters exist for this analysis! So no expansion to be done. Just evaluate the signal directly.
             # Note: there are some shape issues, though. When we use parameters that have been fitted to samples, those
             # fitted parameters have an extra 0 dimension, i.e. the sample dimension (one parameter for each sample). This
             # is missing when there are no nuisance parameters, so we need to add it.
-            pars2d, did_2d_expand = c.atleast_2d(signal,report=True)
-            expanded_pars = c.deep_expand_dims(pars2d,axis=0)
+            expanded_pars = c.deep_expand_dims(signal,axis=0)
             print("signal:", signal)
-            print("pars2d:", pars2d)
             print("expanded_pars:", expanded_pars)
             # Compute -2*log_prob
             joint = JointDistribution(self.analyses.values(),expanded_pars)
         else:
-            s = tf.cast(tf.concat(parlist,axis=-1),dtype=c.TFdtype)
-            s_0 = c.cat_pars_2d(interest) # stacked interest parameter values at expansion point
-            theta_0 = c.cat_pars_2d(nuisance) # stacked nuisance parameter values at expansion point
+            par_shapes = self.parameter_shapes()
+            s, s_bshape = c.cat_pars_to_tensor(parlist,par_shapes) 
+            s_0, s0_bshape = c.cat_pars_to_tensor(interest,par_shapes) # stacked interest parameter values at expansion point
+            theta_0, t0_bshape = c.cat_pars_to_tensor(nuisance,par_shapes) # stacked nuisance parameter values at expansion point
 
-            # Squeeze theta_0 to 2D if it is too big (due to input parameter batch shape)
-            theta_0 = c.squeeze_to(theta_0,d=2) 
+            print("s_bshape:", s_bshape)
+            print("s0_bshape:", s0_bshape)
+            print("t0_bshape:", t0_bshape)
+ 
+            print("s.shape:", s.shape)
+            print("s_0.shape:", s_0.shape)
+            print("theta_0.shape:", theta_0.shape)
+            print("A.shape:", A.shape)
+            print("B.shape:", B.shape)
    
             # Analytically profile (find MLEs for) nuisance parameters, under quadratic log-likelihood approximation 
             theta_prof = tf.expand_dims(theta_0 - A,axis=1) - tf.linalg.matvec(tf.expand_dims(B,axis=1),tf.expand_dims(s,axis=0)-s_0)
             #theta_prof = theta_0 - tf.expand_dims(A,axis=1) - tf.linalg.matvec(tf.expand_dims(B,axis=1),tf.expand_dims(s,axis=0)-s_0) # old shapes
             #theta_prof = theta_0 - tf.linalg.matvec(tf.expand_dims(B,axis=1),tf.expand_dims(s,axis=0)-s_0) # Ignoring grad term
 
+            print("theta_prof.shape:", theta_prof.shape)
+
             # de-stack analytically profiled nuisance parameters
-            theta_prof_dict = c.uncat_pars_2d(theta_prof,pars_template=nuisance)
+            theta_prof_dict = c.decat_tensor_to_pars(theta_prof,nuisance,par_shapes,t0_bshape) 
+            
             # Compute -2*log_prob
             joint = JointDistribution(self.analyses.values(),c.deep_merge(signal,theta_prof_dict))
 
@@ -874,4 +872,18 @@ class JointDistribution(tfd.JointDistributionNamed):
                  raise ValueError(msg + ": -1 was detected in shape!")
 
         return out_shape
+
+    def parameter_shapes(self):
+        """Returns a dictionary describing the shapes of all input parameters for each analysis 
+           required to produce a single event of the basic event shape for all component distributions. 
+           
+           Note: There is a similar function built-in, "param_shape", but it doesn't really make 
+           sense for JointDistribution since it only accepts one input 'event_shape', whilst this 
+           can obviously be different across the various component distributions. This function
+           also works for analysis parameters rather than distribution parameters."""
+        param_shapes = {name: a.parameter_shapes() for name,a in self.analyses.items()}
+        return param_shapes
+
+
+
 

jmctf/normal_analysis.py
@@ -108,21 +108,24 @@ class NormalAnalysis(BaseAnalysis):
         Osamples["x"]       = tf.expand_dims(tf.expand_dims(tf.constant(self.x_obs,dtype=c.TFdtype),0),0)
         return Osamples
 
-    def get_interest_parameter_structure(self):
+    def interest_parameter_shapes(self):
         """Get a dictionary describing the structure of the "interesting" parameters in this analysis
-           Basically just the keys of the parameter dictionaries plus dimension of each entry"""
-        return {"mu": 1}
+           Basically just the keys of the parameter dictionaries plus primitive (non-batch dimensions) 
+           shape of each entry"""
+        return {"mu": ()}
 
-    def get_fixed_parameter_structure(self):
+    def fixed_parameter_shapes(self):
         """Get a dictionary describing the structure of the fixed parameters in this analysis
            (i.e. parameters that can be altered along with the signal hypothesis in nuisance
            parameter fits, but which are kept fixed during all fitting.
-           Basically just the keys of the parameter dictionaries plus dimension of each entry"""
+           Basically just the keys of the parameter dictionaries plus primitive (non-batch dimensions) 
+           shape of each entry"""
         return {} # None for this analysis
 
-    def get_nuisance_parameter_structure(self):
+    def nuisance_parameter_shapes(self):
         """Get a dictionary describing the nuisance parameter structure of this analysis.
-           Basically just the keys of the parameter dictionaries plus dimension of each entry"""
+           Basically just the keys of the parameter dictionaries plus primitive (non-batch dimensions) 
+           shape of each entry"""
         return {} # None for this analysis
 
     def get_nuisance_parameters(self,sample_dict,fixed_pars):